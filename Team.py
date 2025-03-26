5482d709dc677c2c298b6f358e36c3b95824c217
MaryuLopez <maryurilopez@cca.edu.co>
2025-03-26 14:59:24 -0500

    Cambios de ejercicio

--- Team.py
+++ Team.py
@@ -1,3 +1,1 @@
 #1. Escribe un programa que le pida al usuario un número entero y diga si es: Positivo y par, Positivo e impar, Negativo , Cero( con este ultimo, el cero, se debe de mostrar numero neutro y par), 
-
-#Holisdsd
