f6242387f2b3bad552800bb5baa63ffc3fcf3886
MaryuLopez <maryurilopez@cca.edu.co>
2025-03-26 14:46:35 -0500

    Cambios de ejercicio

--- Team.py
+++ Team.py
@@ -1,1 +1,15 @@
-#2. Escribe un programa que le pida al usuario un número entero y diga si es: Positivo y par, Positivo e impar, Negativo , Cero( con este ultimo, el cero, se debe de mostrar numero neutro y par), 
+#1. Escribe un programa que le pida al usuario un número entero y diga si es: Positivo y par, Positivo e impar, Negativo , Cero( con este ultimo, el cero, se debe de mostrar numero neutro y par), 
+
+#2. 
+numeroDeAcceso = int(input("Ingrese numero de acceso: "))
+añosDeAntiguedad = int(input("Ingrese sus años de pertenencia en la empresa: "))
+
+huelaDactilar = True
+Admin =  True
+
+if numeroDeAcceso == 1234 and huelaDactilar == True and añosDeAntiguedad >= 5:
+    print("BIENVENIDO")
+else:
+    print("NO PUEDES INGRESAR")
+    
+    
