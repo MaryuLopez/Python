3e8a1362c493a60ed1bf4e06545700a520a841bc
DaniV-08 <danielvivas@cca.edu.co>
2025-03-28 12:19:24 -0500

    condicionales

--- Team.py
+++ Team.py
@@ -1,1 +1,15 @@
-#2. Escribe un programa que le pida al usuario un número entero y diga si es: Positivo y par, Positivo e impar, Negativo , Cero( con este ultimo, el cero, se debe de mostrar numero neutro y par), 
+#2. Escribe un programa que le pida al usuario un número entero y diga si es: Positivo y par, Positivo e impar, Negativo , Cero( con este ultimo, el cero, se debe de mostrar numero neutro y par),
+variable = int(input("Ingrese un número: ")) 
+print(variable)
+if variable > 0 and variable % 2 == 0:
+    print("El número es positivo y par")
+elif variable > 0 and variable % 2 !=0:
+    print("el numero es positivo e impar")
+elif variable < 0 and variable % 2 != 0:
+    print("El número es negativo e impar")
+elif variable < 0 and variable % 2 == 0:
+    print("El número es negativo y par")
+elif variable == 0:
+    print("El número es neutro y par")
+else:
+    print("El número es neutro y impar")
--- daniel.py
+++ daniel.py
@@ -1,1 +1,18 @@
-# Declara 5 variables con diferentes tipos de datos
+# # Declara 5 variables con diferentes tipos de datos
+# variable1 = 10
+# variable2 = 10.5
+# variable3 = "Hola"
+# variable4 = True
+# variable5 = 10j
+# print(variable1,variable2,variable3,variable4,variable5)
+# if variable1 >0: 
+#     print("La variable1 es positiva")
+# else: 
+#     print("La variable1 es negativa")
+
+variable6 = int(input("Ingrese un número: "))
+print(variable6)
+if variable6 % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
